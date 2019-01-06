d88179e88b5d31fbc3450d85ebc199b57bbd552c
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2019-01-06 18:41:29 +0100

    Gen doc.

--- src/Data/SpreadSheet/Internal/Gen.hs
+++ src/Data/SpreadSheet/Internal/Gen.hs
@@ -1,3 +1,7 @@
+-- | Conjunto de generadore e instancias de
+-- 'Test.QuickCheck.Arbitrary' para
+-- 'Test.QuickCheck.QuickCheck'. Útiles para el ejemplo y para los
+-- módulos de test.
 module Data.SpreadSheet.Internal.Gen where
 
 
@@ -14,22 +18,29 @@ import Test.QuickCheck.Function
 
 import Control.Applicative (liftA2)
 
+-- | Instancia para los naturales, igual que para 'Integer' pero solo
+-- de sus valores absolutos.
 instance Arbitrary Natural where
   arbitrary = fromIntegral . abs <$> (arbitrary :: Gen Integer)
   -- shrink nat = fromIntegral . abs <$> shrink (toInteger nat) :: [Natural]
 
+-- | Se deriva la instancia de 'Natural'
 instance Arbitrary Range where
   arbitrary = liftA2 Range arbitrary arbitrary
 
+-- | Se deriva la instancia de @[(Pos, a)]@
 instance Arbitrary a => Arbitrary (SpreadSheet a) where
   arbitrary = fromList <$> arbitrary
 
+-- | Tomada a partir de la lista de valores. @[a]@
 instance CoArbitrary a => CoArbitrary (SpreadSheet a) where
   coarbitrary = coarbitrary . toListValues
 
+-- | Tomada a partir de los 'Integer'
 instance CoArbitrary Natural where
   coarbitrary = coarbitrary . toInteger
 
+-- | Se obtiene a través de los 'Integer'
 instance Function Natural where
   function = functionMap toInteger fromInteger
 
@@ -39,12 +50,19 @@ instance Function Natural where
 -- instance CoArbitrary Cell where
 --   coarbitrary = coarbitraryShow
 
+-- | Tomada a partir de las listas de valores con posiciones.
 instance Function a => Function (SpreadSheet a) where
   function = functionMap (Map.fromList . toListPosValues) (fromList . Map.toList)
 
+-- | Usando 'genDay'.
 instance Arbitrary Day where
   arbitrary = genDay
 
+-- | Distribuye de forma equitativa la generación de valores:
+-- - 'Data.SpreadSheet.Cell.CNumber'
+-- - 'Data.SpreadSheet.Cell.CString'
+-- - 'Data.SpreadSheet.Cell.CBool'
+-- - 'Data.SpreadSheet.Cell.CDay'
 instance Arbitrary Cell where
   -- shrink = const []
   arbitrary = frequency $
@@ -54,7 +72,7 @@ instance Arbitrary Cell where
     , (1, CDay <$> arbitrary)
     ]
 
-
+-- | Generador de fechas próximas al final de 2018.
 genDay :: Gen Day
 genDay = do
   n <- (58470 +) <$> arbitrary
